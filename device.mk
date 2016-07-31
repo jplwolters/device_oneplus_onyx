6cbb3c663c05833f7f858a4b3eb4316a4d584408
umang96 <umangleekha3@gmail.com>
2016-08-01 02:22:53 +0530

    onyx: Use half-res bootanimation
    * This makes boot animation smoother
    * Cpu load is also reduced when booting
    
    Change-Id: I68164d57e4aee988def097843c29aded9bd63d6a

--- device.mk
+++ device.mk
@@ -20,6 +20,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 # Boot animation
+TARGET_BOOTANIMATION_HALF_RES := true
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
